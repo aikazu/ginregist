5d0d747f7959d35e54d39fe992a3376846eb42e7
aikazu <devas74tor@gmail.com>
2019-10-28 20:59:18 +0700

    Base Final

--- models.article.go
+++ models.article.go
@@ -8,35 +8,24 @@ type article struct {
 	Content string `json:"content"`
 }
 
-// For this demo, we're storing the article list in memory
-// In a real application, this list will most likely be fetched
-// from a database or from static files
-var articleList = []article{
-	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
-	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
-}
+var articleList = []article{}
 
-// Return a list of all the articles
 func getAllArticles() []article {
 	return articleList
 }
 
-// Fetch an article based on the ID supplied
 func getArticleByID(id int) (*article, error) {
 	for _, a := range articleList {
 		if a.ID == id {
 			return &a, nil
 		}
 	}
-	return nil, errors.New("Article not found")
+	return nil, errors.New("Artikel tidak ditemukan")
 }
 
-// Create a new article with the title and content provided
 func createNewArticle(title, content string) (*article, error) {
-	// Set the ID of a new article to one more than the number of articles
 	a := article{ID: len(articleList) + 1, Title: title, Content: content}
 
-	// Add the article to the list of articles
 	articleList = append(articleList, a)
 
 	return &a, nil
--- models.user.go
+++ models.user.go
@@ -10,19 +10,9 @@ type user struct {
 	Password string `json:"-"`
 }
 
-// For this demo, we're storing the user list in memory
-// We also have some users predefined.
-// In a real application, this list will most likely be fetched
-// from a database. Moreover, in production settings, you should
-// store passwords securely by salting and hashing them instead
-// of using them as we're doing in this demo
 var userList = []user{
-	user{Username: "user1", Password: "pass1"},
-	user{Username: "user2", Password: "pass2"},
-	user{Username: "user3", Password: "pass3"},
-}
+	user{Username: "admin", Password: "admin"}}
 
-// Check if the username and password combination is valid
 func isUserValid(username, password string) bool {
 	for _, u := range userList {
 		if u.Username == username && u.Password == password {
@@ -32,13 +22,11 @@ func isUserValid(username, password string) bool {
 	return false
 }
 
-// Register a new user with the given username and password
-// NOTE: For this demo, we
 func registerNewUser(username, password string) (*user, error) {
 	if strings.TrimSpace(password) == "" {
-		return nil, errors.New("The password can't be empty")
+		return nil, errors.New("Password tidak bole kosong")
 	} else if !isUsernameAvailable(username) {
-		return nil, errors.New("The username isn't available")
+		return nil, errors.New("Username telah terpakai")
 	}
 
 	u := user{Username: username, Password: password}
@@ -48,7 +36,6 @@ func registerNewUser(username, password string) (*user, error) {
 	return &u, nil
 }
 
-// Check if the supplied username is available
 func isUsernameAvailable(username string) bool {
 	for _, u := range userList {
 		if u.Username == username {
